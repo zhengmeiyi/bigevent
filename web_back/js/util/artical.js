2aa51da8cfdf4cc3f7cdf74d6a353fe43e714c06
meiyi zheng <524875323@qq.com>
2020-02-06 22:26:06 +0800

    文章列表-筛选

--- web_back/js/util/artical.js
+++ web_back/js/util/artical.js
@@ -1,7 +1,7 @@
 // 与文章有关
 var artical = {
-    get: function (page) {
-        return $.get(APILIST.artical_get,{'page':page})
+    get: function (page,type,atatus) {
+        return $.get(APILIST.artical_get,{'page':page,'type':type,'state':status})
     },
     // add: function (name, slug) {
     //     return $.post(APILIST.category_add, {
